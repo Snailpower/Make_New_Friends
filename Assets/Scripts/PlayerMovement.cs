5b06005ceda68b52bd927a5604c4baeedae94e86
JAMMU\Jammu <mummo94@hotmail.com>
2018-01-26 20:50:56 +0200

    Basic controls for 4 players and fixed git

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public string playerID = "P1";
+    public float speedReducerValue = 0.5f;
+
+	void Update () {
+        float moveValueX = Input.GetAxis("Horizontal_" + playerID) * speedReducerValue;
+        float moveValueY = Input.GetAxis("Vertical_" + playerID) * speedReducerValue;
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + moveValueX, gameObject.transform.position.y + moveValueY, gameObject.transform.position.z);
+	}
+}
