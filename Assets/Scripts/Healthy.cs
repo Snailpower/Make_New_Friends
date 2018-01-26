a996471a745ad9e031713ea6fa7e744652357ad7
MemebookPro\Tom Hoevers <saltynippleman@gmail.com>
2018-01-26 22:24:27 +0200

    Added my test scene with a basic flocking thingy

---
+++ Assets/Scripts/Healthy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthy : MonoBehaviour {
+
+    public GameObject target;
+
+    public GameObject manager;
+    public Vector2 location = Vector2.zero;
+    public Vector2 velocity;
+
+    private Vector2 goalpos = Vector2.zero;
+    private Vector2 currentForce;
+
+    private Vector2 seek(Vector2 target)
+    {
+        return (target - location);
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        target = GameObject.FindGameObjectWithTag("Finish");
+
+        velocity = new Vector2(Random.Range(0.1f, 0.1f), Random.Range(0.1f, 0.1f));
+        location = new Vector2(transform.position.x, transform.position.y);
+		
+	}
+
+    private void ApplyForce(Vector2 f)
+    {
+        Vector3 force = new Vector3(f.x, f.y, 0);
+        GetComponent<Rigidbody2D>().AddForce(force);
+        Debug.DrawRay(transform.position, force, Color.white);
+    }
+
+    private void Flock()
+    {
+        location = transform.position;
+        velocity = GetComponent<Rigidbody2D>().velocity;
+
+        Vector2 gl;
+        gl = seek(goalpos);
+        currentForce = gl;
+        currentForce = currentForce.normalized;
+
+        ApplyForce(currentForce);
+    }
+
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Flock();
+        goalpos = target.transform.position;
+		
+	}
+}
---
+++ Assets/Scripts/HealthyUnitManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthyUnitManager : MonoBehaviour {
+
+    public List<GameObject> unitsHealthy;
+
+    public GameObject unitHealthyPrefab;
+
+    public int healthyAmount = 25;
+
+    public Vector3 range = new Vector3(10, 10, 10);
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(this.transform.position, range * 2);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(this.transform.position, Vector3.one * 0.2f);
+
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        unitsHealthy = new List<GameObject>(healthyAmount);
+
+        for (int i = 0; i <= healthyAmount; i++)
+        {
+            Vector3 unitPos = new Vector3(Random.Range(-range.x, range.x),
+                                          Random.Range(-range.y, range.y),
+                                          Random.Range(0, 0));
+
+            GameObject addedUnit = Instantiate(unitHealthyPrefab, this.transform.position + unitPos, Quaternion.identity);
+
+            addedUnit.GetComponent<Healthy>().manager = this.gameObject;
+
+            unitsHealthy.Add(addedUnit);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
