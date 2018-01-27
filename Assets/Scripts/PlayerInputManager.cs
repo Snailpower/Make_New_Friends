4b057bb6d9462c821c19efef49cac5ec83a6416f
JAMMU\Jammu <mummo94@hotmail.com>
2018-01-27 16:25:16 +0200

    A and B button configed to willful and flocking

---
+++ Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour {
+
+    private InfectedUnitManager infectedUnitManager;
+
+    private void Start()
+    {
+        infectedUnitManager = gameObject.GetComponent<Infected>().manager.GetComponent<InfectedUnitManager>();
+    }
+    void Update () {
+        if(Input.GetButtonDown("ButtonA_" + gameObject.GetComponent<PlayerMovement>().playerID))
+        {
+            print("ASDAA");
+            infectedUnitManager.followFlockingRules = !infectedUnitManager.followFlockingRules;
+        }
+        if (Input.GetButtonDown("ButtonB_" + gameObject.GetComponent<PlayerMovement>().playerID))
+        {
+            print("ASDBBB");
+            infectedUnitManager.willful = !infectedUnitManager.willful;
+        }
+    }
+}
