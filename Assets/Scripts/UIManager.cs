8037e51865656ae791faed0feec611c477ce3cc3
JAMMU\Jammu <mummo94@hotmail.com>
2018-01-27 21:01:09 +0200

    Very shitty UI code boi

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,15 +10,24 @@ public class UIManager : MonoBehaviour {
     public int player3MinionCount = 0;
     public int player4MinionCount = 0;
     private string originalPlayer1Text, originalPlayer2Text, originalPlayer3Text, originalPlayer4Text;
-
+    private Text playerDiedText;
+    private GameObject playerDiedMenu;
     private Text player1MinionCountText, player2MinionCountText, player3MinionCountText, player4MinionCountText;
+    private Vector3 playerDiedMenuOriginalPosition;
+    public float howLongWillPlayerDiedMenuShow = 2f;
+    public float howLongPlayerHasTimeToGatherMinions = 15f;
+
+    private bool canShowP1DiedScreen = true, canShowP2DiedScreen = true, canShowP3DiedScreen = true, canShowP4DiedScreen = true;
 
     private void Start()
     {
+        playerDiedMenu = GameObject.Find("PlayerDiedMenu");
+        playerDiedMenuOriginalPosition = playerDiedMenu.transform.position;
         player1MinionCountText = GameObject.Find("Player1MinionCount").GetComponent<Text>();
         player2MinionCountText = GameObject.Find("Player2MinionCount").GetComponent<Text>();
         player3MinionCountText = GameObject.Find("Player3MinionCount").GetComponent<Text>();
         player4MinionCountText = GameObject.Find("Player4MinionCount").GetComponent<Text>();
+        playerDiedText = GameObject.Find("PlayerDiedText").GetComponent<Text>();
         originalPlayer1Text = player1MinionCountText.text;
         originalPlayer2Text = player2MinionCountText.text;
         originalPlayer3Text = player3MinionCountText.text;
@@ -55,6 +64,22 @@ public class UIManager : MonoBehaviour {
                 player4MinionCount++;
             }
         }
+        if (howLongPlayerHasTimeToGatherMinions >= 0)
+            howLongPlayerHasTimeToGatherMinions -= Time.deltaTime;
+        if(howLongPlayerHasTimeToGatherMinions <= 0)
+        {
+            print("Show Shit");
+            if (player1MinionCount <= 1 && canShowP1DiedScreen)
+                ShowPlayerDiedMenu(0);
+            else if (player2MinionCount <= 1 && canShowP2DiedScreen)
+                ShowPlayerDiedMenu(1);
+            else if (player3MinionCount <= 1 && canShowP3DiedScreen)
+                ShowPlayerDiedMenu(2);
+            else if (player4MinionCount <= 1 && canShowP4DiedScreen)
+                ShowPlayerDiedMenu(3);
+        }
+        print("ASD: " + howLongPlayerHasTimeToGatherMinions);
+        
     }
     private void updateMinionCountTexts()
     {
@@ -62,5 +87,45 @@ public class UIManager : MonoBehaviour {
         player2MinionCountText.text = originalPlayer2Text + player2MinionCount.ToString();
         player3MinionCountText.text = originalPlayer3Text + player3MinionCount.ToString();
         player4MinionCountText.text = originalPlayer4Text + player4MinionCount.ToString();
+    }
+
+    private void ShowPlayerDiedMenu(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                canShowP1DiedScreen = false;
+                break;
+            case 1:
+                canShowP2DiedScreen = false;
+                break;
+            case 2:
+                canShowP3DiedScreen = false;
+                break;
+            case 3:
+                canShowP4DiedScreen = false;
+                break;
+        }
+
+        if(howLongWillPlayerDiedMenuShow > 0)
+        {
+            playerDiedMenu.transform.localPosition = Vector3.zero;
+            playerDiedText.text = "Player " + index + " died";
+            howLongWillPlayerDiedMenuShow -= Time.deltaTime;
+            print("APSODKPOK" + index + " : " + howLongWillPlayerDiedMenuShow);
+        }
+        else if (howLongWillPlayerDiedMenuShow <= 0)
+        {
+            howLongWillPlayerDiedMenuShow = 2;
+            HidePlayerDiedMenu();
+            return;
+        }
+            
+
+
+    }
+    private void HidePlayerDiedMenu()
+    {
+        playerDiedMenu.transform.position = playerDiedMenuOriginalPosition;
     }
 }
