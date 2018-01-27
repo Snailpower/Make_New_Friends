bd98d25fa914c094cc2c8c41836591964200d87a
JAMMU\Jammu <mummo94@hotmail.com>
2018-01-27 11:54:37 +0200

    Made prefabs

--- Assets/Scripts/PlayerTransferManager.cs
+++ Assets/Scripts/PlayerTransferManager.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTransferManager : MonoBehaviour {
-    
+public class PlayerTransferManager : MonoBehaviour
+{
+
+    public int playerIndex = 0;
+
     public void MovePlayerToNearest()
     {
-        GameObject[] myInfectedObjects = GameObject.FindGameObjectsWithTag("TEMPTAG");
-        GameObject nearestInfected ;
+        GameObject[] myInfectedObjects = GameObject.FindGameObjectsWithTag("Infected");
+        GameObject nearestInfected;
         if (myInfectedObjects.Length == 0 || myInfectedObjects == null)
             PlayerKilled();
         nearestInfected = myInfectedObjects[0];
         foreach (GameObject infected in myInfectedObjects)
         {
-            if (Vector3.Distance(infected.transform.position, gameObject.transform.position) < Vector3.Distance(gameObject.transform.position, nearestInfected.transform.position))
-                nearestInfected = infected;
+            //if (infected.index == playerIndex)
+                if (Vector3.Distance(infected.transform.position, gameObject.transform.position) < Vector3.Distance(gameObject.transform.position, nearestInfected.transform.position))
+                    nearestInfected = infected;
         }
         nearestInfected.AddComponent<PlayerMovement>();
         nearestInfected.AddComponent<PlayerHealthManager>();
